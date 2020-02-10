4f65fba6cf52492593cdb8b1afa6c8bd84f9f269
Javi-RS <javirodrigues@gmail.com>
2020-02-10 22:41:19 +0000

    recovery

--- Assignment3/test_app.py
+++ Assignment3/test_app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def fun():
     name = request.args.get("name", "World")
-    #return jsonify({"response":"Hello " + name})
-    return "Please, specify a route"
-    
-
-@app.route('/get')
-def create():
-    name = request.args.get("name", "World")
     return jsonify({"response":"Hello " + name})
+#    return "Please, specify a route"
+#    
 
-@app.route('/post')
-def create():
-    name = request.args.get("name", "World")
-    
-    return jsonify({"result":"Success"})
+#@app.route('/get')
+#def create():
+#    name = request.args.get("name", "World")
+#    return jsonify({"response":"Hello " + name})
+
+#@app.route('/post')
+#def create():
+#    name = request.args.get("name", "World")
+#    
+#    return jsonify({"result":"Success"})
