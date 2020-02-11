3db884fd8d74a469ab2c9028e022d8c61330f08a
Javi-RS <javirodrigues@gmail.com>
2020-02-11 05:00:03 +0000

    2 GET requests working well

--- Assignment3/test_app.py
+++ Assignment3/test_app.py
@@ -1,56 +1,91 @@
-#import sqlite3
+import sqlite3
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/', methods=['GET'])
 def fun():
 #  name = request.args.get("name", "World")
 #  return jsonify({"response":"Hola " + name})
-  return "Please, specify a route"
+  return "Please, specify a route: read or write"
 
-@app.route('/read/all', methods=['GET']))
+@app.route('/read/all', methods=['GET'])
 def read_all():
-  return jsonify(books)
 
-#  DATABASE = os.path.join(app.root_path, 'Assignment2.db')
-#  table = request.args.get("table")
-#  conn = sqlite3.connect('DATABASE')
-#  cur = conn.cursor()
-#  cur.execute('SELECT * FROM "table"  ORDER BY name')
-#  people = cur.fetchall()
-#  for id in table:
-#    print(f'{name} {}')
+  query_parameters = request.args
+  table = query_parameters.get('table')
 
-#@app.route('/update/', methods=['GET, 'POST']))
-#def updatee():
-#    if validate_on_submit():
-#      flash('Data introduced correctly!', 'success')
-#    else:
-#      flash('wrong data. Please check data introduced')
-#  return render_template('write.html',title='Writing')
+  conn = sqlite3.connect('Assignment2.db')
+  conn.row_factory = dict_factory
+  cur = conn.cursor()
 
-#@app.route('/delete/')
-#def delete():
+  if table == 'researchers':
+    all = cur.execute('SELECT * FROM researchers;').fetchall()
+  elif table == 'projects':
+    all = cur.execute('SELECT * FROM projects;').fetchall()
+  elif table == 'departments':
+    all = cur.execute('SELECT * FROM departments;').fetchall()
+  elif table == 'tasks':
+    all = cur.execute('SELECT * FROM tasks;').fetchall()
+  elif table == 'equipment':
+     all = cur.execute('SELECT * FROM equipment;').fetchall()
+  else:
+    return "Please, specify the table to show"
+  return jsonify(all)
 
-#@app.route('/create/')
-#def create():
+@app.errorhandler(404)
+def page_not_found(e):
+  return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+@app.route('/read_researchers', methods=['GET'])
+def read_filter():
+  query_parameters = request.args
+
+  id = query_parameters.get('id')
+  first_name = query_parameters.get('first_name')
+  last_name = query_parameters.get('last_name')
+  phone = query_parameters.get('phone')
+  department_id = query_parameters.get('department_id')
+  
+
+  query = "SELECT * FROM researchers WHERE"
+  to_filter = []
+
+  if id:
+    query += ' id=? AND'
+    to_filter.append(id)
+
+  if first_name:
+    query += ' first_name=? AND'
+    to_filter.append(published)
+
+  if last_name:
+    query += ' last_name=? AND'
+    to_filter.append(author)
+
+  if phone:
+    query += ' phone=? AND'
+    to_filter.append(phone)
+
+  if department_id:
+    query += ' department_id=? AND'
+    to_filter.append(department_id)
+
+  if not (id or first_name or last_name or phone or department_id):
+    return page_not_found(404)
+
+  query = query[:-4] + ';'
+
+  conn = sqlite3.connect('Assignment2.db')
+  conn.row_factory = dict_factory
+  cur = conn.cursor()
+
+  results = cur.execute(query, to_filter).fetchall()
+
+  return jsonify(results)
