e7ec2bad2f5d8db172a4c49b2a3078334e690ba5
Javi-RS <55458214+Javi-RS@users.noreply.github.com>
2021-04-14 23:18:32 -0400

    Update library versions
    Use scikit-learn==0.22 to load the SVM model properly using joblib

--- final_project/python_install.sh
+++ final_project/python_install.sh
@@ -4,17 +4,18 @@ pip install virtualenv
 virtualenv foss_groupF
 source foss_groupF/bin/activate
 
-pip install flask==1.1.2
-pip install werkzeug.utils==1.0.1
-pip install scikit-learn==0.21.2
-pip install numpy==1.18.2
-pip install pandas==1.0.3
-pip install seaborn==0.10.0
-#pip install PIL==1.1.6
-pip install pillow==8.0.1 #Pillow is a maintained fork of PIL
-pip install os==0.2.14
 pip install base64
+pip install flask==1.1.2
+pip install matplotlib==3.3.2
+pip install numpy==1.19.5
 pip install opencsv==4.2.0.34
-pip install opencv-contrib-python
+pip install opencv-contrib-python==4.5.1.48
+pip install os==0.2.14
+pip install pandas==1.1.3
+#pip install PIL==1.1.6
+pip install pillow==8.2.0 #Pillow is a maintained fork of PIL
+pip install seaborn==0.11.0
+pip install scikit-learn==0.22
+pip install werkzeug.utils==1.0.1
 
 
